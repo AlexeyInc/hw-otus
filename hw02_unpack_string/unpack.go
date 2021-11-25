c3922349c036d6085b0892b2fd2918f04d64689f
Alexey Ovsyannikov <AOvsyannikov2@alfabank.ru>
2021-11-25 15:40:02 +0200

    hw 2 done
    Main task and task with asterisk

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,135 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+const charSlash = 92
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputStr string) (string, error) {
+	var resultStr string
+	if isBacktickString(inputStr) {
+		errorMsg := validateBacktickString(inputStr)
+		if errorMsg != nil {
+			return "", errorMsg
+		}
+		resultStr = unpackBacktickString(inputStr)
+	} else {
+		errorMsg := validateQuotedString(inputStr)
+		if errorMsg != nil {
+			return "", errorMsg
+		}
+		resultStr = unpackQuotedString(inputStr)
+	}
+	return resultStr, nil
+}
+
+func isBacktickString(str string) bool {
+	return strings.ContainsAny(str, "\\")
+}
+
+func validateBacktickString(str string) error {
+	for i := range str {
+		if str[i] == '\\' && i+1 < len(str)-1 {
+			shieldingChar := str[i+1]
+
+			if (shieldingChar < 48 || shieldingChar > 57) && shieldingChar != 92 {
+				return ErrInvalidString
+			}
+		}
+	}
+	return nil
+}
+
+func unpackBacktickString(inputStr string) string {
+	var result strings.Builder
+	i := 0
+	for ; i < len(inputStr); i++ {
+		if isCharSlash(inputStr[i]) {
+			addNewStr := makeShielding(i, inputStr)
+			i++
+			result.WriteString(addNewStr)
+		} else if !unicode.IsDigit([]rune(inputStr)[i]) {
+			result.WriteString(string(inputStr[i]))
+		}
+	}
+	return result.String()
+}
+
+func isCharSlash(s byte) bool {
+	return s == charSlash
+}
+
+func makeShielding(i int, str string) string {
+	if i+1 > len(str)-1 {
+		return ""
+	}
+	if i+2 > len(str)-1 || !unicode.IsDigit([]rune(str)[i+2]) {
+		return string(str[i+1])
+	}
+
+	var resultStr string
+	if numRep, err := strconv.Atoi(string(str[i+2])); err == nil {
+		resultStr = strings.Repeat(string(str[i+1]), numRep)
+	} else {
+		return ""
+	}
+	return resultStr
+}
+
+func validateQuotedString(str string) error {
+	if len(str) == 0 {
+		return nil
+	}
+	if unicode.IsDigit([]rune(str)[0]) {
+		return ErrInvalidString
+	}
+	if !requireWithoutNumbers(str) {
+		return ErrInvalidString
+	}
+	return nil
+}
+
+func unpackQuotedString(inputStr string) string {
+	var resultStr strings.Builder
+
+	for i, v := range inputStr {
+		if unicode.IsDigit(v) {
+			if numRep, err := strconv.Atoi(string(inputStr[i])); err == nil {
+				strRep := string(inputStr[i-1])
+				resultStr.WriteString(strings.Repeat(strRep, numRep))
+			}
+		} else if isNextCharNotDigit(i, inputStr) {
+			resultStr.WriteString(string(inputStr[i]))
+		}
+	}
+	return resultStr.String()
+}
+
+func requireWithoutNumbers(str string) bool {
+	strArr := []rune(str)
+	countDigit := 0
+
+	for i := 0; i < len(strArr); i++ {
+		if unicode.IsDigit(strArr[i]) {
+			countDigit++
+		} else {
+			countDigit = 0
+		}
+		if countDigit > 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func isNextCharNotDigit(i int, str string) bool {
+	if i+1 > len(str)-1 {
+		return true
+	}
+	return !unicode.IsDigit([]rune(str)[i+1])
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,12 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
 		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `check\*`, expected: `check*`},
+		{input: `check\\\\`, expected: `check\\`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +36,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "a123b"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {

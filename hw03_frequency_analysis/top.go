3bccbee09449cc9541ad8c347f66cf554e399c64
Alexey Ovsyannikov <AOvsyannikov2@alfabank.ru>
2021-12-05 22:40:02 +0200

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const maxNumOfWords int = 10
+
+type wordFrequency struct {
+	Key   string
+	Value int
+}
+
+func Top10(input string) (result []string) {
+	splitedInput := strings.Fields(input)
+
+	wordsFreq := calcWordsFreq(splitedInput)
+
+	sortedWords := sortWordsByFreq(wordsFreq)
+
+	for i := 0; i < len(sortedWords); i++ {
+		if maxNumOfWords < i+1 {
+			break
+		}
+		result = append(result, sortedWords[i].Key)
+	}
+	return result
+}
+
+func calcWordsFreq(words []string) (wordsFreq []wordFrequency) {
+	wordsMap := make(map[string]int)
+	for _, k := range words {
+		wordsMap[k]++
+	}
+	for k, v := range wordsMap {
+		wordsFreq = append(wordsFreq, wordFrequency{k, v})
+	}
+	return wordsFreq
+}
+
+func sortWordsByFreq(words []wordFrequency) []wordFrequency {
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].Value == words[j].Value {
+			frstWord := []rune(words[i].Key)
+			secWord := []rune(words[j].Key)
+			var shorterWord []rune
+			if len(frstWord) < len(secWord) {
+				shorterWord = frstWord
+			} else {
+				shorterWord = secWord
+			}
+			for i := 0; i < len(shorterWord); i++ {
+				if frstWord[i] != secWord[i] {
+					return frstWord[i] < secWord[i]
+				}
+			}
+			return len(frstWord) < len(secWord)
+		}
+		return words[i].Value > words[j].Value
+	})
+	return words
 }

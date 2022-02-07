06dbf1eb93a9ff9b46c387b550812509cac87135
Alexey Ovsianicoff <allah420@protonmail.com>
2022-02-07 21:07:47 +0200

    Updated test
    Compare files by each byte

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"testing"
 
@@ -43,36 +45,47 @@ func TestCopyErrors(t *testing.T) {
 		fOutSize := getFileSize(tempOutFile)
 
 		require.Equal(t, fInputSize, fOutSize)
+		require.True(t, compareFiles(realInputFile, tempOutFile))
 	})
 
-	limits := []int64{10, 1000, 1025, 10000}
+	limits := []struct {
+		value       int64
+		resFilePath string
+	}{
+		{10, "testdata/out_offset0_limit10.txt"},
+		{1000, "testdata/out_offset0_limit1000.txt"},
+		{10000, "testdata/out_offset0_limit10000.txt"},
+		{1025, "testdata/out_offset0_limit1025.txt"},
+	}
 
 	for _, limit := range limits {
 		t.Run("Check behaviour with 'limit' value", func(t *testing.T) {
 			defer helperRemoveFile(t, tempOutFile)
 
-			Copy(realInputFile, tempOutFile, 0, limit)
+			Copy(realInputFile, tempOutFile, 0, limit.value)
 
 			fInputSize := getFileSize(realInputFile)
-			if limit > fInputSize {
-				limit = fInputSize
+			if limit.value > fInputSize {
+				limit.value = fInputSize
 			}
 
 			fOutSize := getFileSize(tempOutFile)
 
-			require.Equal(t, limit, fOutSize)
+			require.Equal(t, limit.value, fOutSize)
+			require.True(t, compareFiles(tempOutFile, limit.resFilePath))
 		})
 	}
 
 	offsetAndLimits := []struct {
-		offset int64
-		limit  int64
+		offset      int64
+		limit       int64
+		resFilePath string
 	}{
-		{100, 1000},
-		{6000, 10000},
-		{-1, 1000},
-		{100, -1},
-		{-100, -100},
+		{100, 1000, "testdata/out_offset100_limit1000.txt"},
+		{6000, 10000, "testdata/out_offset6000_limit1000.txt"},
+		{-1, 1000, "testdata/out_offset0_limit1000.txt"},
+		{100, -1, "testdata/out_offset100_limit0.txt"},
+		{-100, -100, "testdata/out_offset0_limit0.txt"},
 	}
 
 	for _, ol := range offsetAndLimits {
@@ -96,6 +109,7 @@ func TestCopyErrors(t *testing.T) {
 			fOutSize := getFileSize(tempOutFile)
 
 			require.Equal(t, expectedSize, fOutSize)
+			require.True(t, compareFiles(tempOutFile, ol.resFilePath))
 		})
 	}
 }
@@ -111,4 +125,40 @@ func getFileSize(fPath string) int64 {
 		return 0
 	}
 	return info.Size()
+}
+
+func compareFiles(file1, file2 string) bool {
+	var chunkSize = 1024
+
+	f1, err := os.Open(file1)
+	if err != nil {
+		return false
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		return false
+	}
+	defer f2.Close()
+
+	for {
+		b1 := make([]byte, chunkSize)
+		_, err1 := f1.Read(b1)
+
+		b2 := make([]byte, chunkSize)
+		_, err2 := f2.Read(b2)
+
+		if err1 != nil || err2 != nil {
+			if err1 == io.EOF && err2 == io.EOF {
+				return true
+			} else if err1 == io.EOF || err2 == io.EOF {
+				return false
+			}
+		}
+
+		if !bytes.Equal(b1, b2) {
+			return false
+		}
+	}
 }

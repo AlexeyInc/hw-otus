10addf5c5bf7db630649ac7c1012341db6a2a0cd
Alexey Ovsianicoff <allah420@protonmail.com>
2022-02-07 20:14:05 +0200

    hm 07 completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"io"
+	"io/fs"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -10,6 +16,103 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fSrc, fiErr := os.OpenFile(fromPath, os.O_RDONLY, 0o444)
+	if fiErr != nil {
+		return fiErr
+	}
+	defer fSrc.Close()
+
+	fDest, foErr := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0o644)
+	if foErr != nil {
+		return foErr
+	}
+	defer fDest.Close()
+
+	var fSrcInfo os.FileInfo
+	v, err := fSrc.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	fSrcInfo = v
+
+	if err := addOffset(fSrc, fSrcInfo, offset); err != nil {
+		return err
+	}
+
+	copyErr := copyWithProgressBar(fSrc, fDest, fSrcInfo, limit)
+	if copyErr != nil {
+		return copyErr
+	}
+
 	return nil
+}
+
+func addOffset(file *os.File, fInfo os.FileInfo, offset int64) error {
+	if offset > 0 {
+		if offset > fInfo.Size() {
+			return ErrOffsetExceedsFileSize
+		}
+		if _, err := file.Seek(offset, 0); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func copyWithProgressBar(fSrc, fDest *os.File, fSrcInfo fs.FileInfo, limitToCopy int64) error {
+	var copyingCompleted bool
+	var defBufChunk, barCount int64 = 1024, 0
+
+	if limitToCopy > 0 && defBufChunk > limitToCopy {
+		defBufChunk = limitToCopy
+		copyingCompleted = true
+		barCount = 1
+	} else {
+		fileSize := fSrcInfo.Size() - offset
+		if limitToCopy > 0 && fileSize > limitToCopy {
+			setBarCount(&barCount, limitToCopy, defBufChunk)
+		} else {
+			setBarCount(&barCount, fileSize, defBufChunk)
+		}
+	}
+
+	buf := make([]byte, defBufChunk)
+	bar := pb.New64(barCount).Start()
+	bytesRead := 0
+
+	for {
+		n, err := fSrc.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+		bytesRead += n
+
+		if _, err := fDest.Write(buf[:n]); err != nil {
+			return err
+		}
+		bar.Increment()
+		time.Sleep(time.Millisecond * 100)
+		if copyingCompleted {
+			break
+		}
+
+		if limitToCopy > 0 && int64(bytesRead)+defBufChunk > limitToCopy {
+			buf = make([]byte, limitToCopy-int64(bytesRead))
+			copyingCompleted = true
+		}
+	}
+
+	bar.Finish()
+
+	return nil
+}
+
+func setBarCount(barCount *int64, fSize, buffer int64) {
+	*barCount = fSize / buffer
+	if fSize%buffer != 0 {
+		*barCount++
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,114 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+func TestCopyErrors(t *testing.T) {
+	realInputFile := "testdata/input.txt"
+	notExistInputFile := "testdata/dummyInput.txt"
+	tempOutFile := "testdata/dummyOut.txt"
+
+	t.Run("Should return err if 'input' file not exists", func(t *testing.T) {
+		err := Copy(notExistInputFile, "", 0, 0)
+
+		require.Error(t, err)
+	})
+
+	t.Run("Should NOT return err if 'out' file not exists", func(t *testing.T) {
+		defer helperRemoveFile(t, tempOutFile)
+
+		err := Copy(realInputFile, tempOutFile, 0, 0)
+
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("Should return err if 'offset' bigger than 'input' file size", func(t *testing.T) {
+		defer helperRemoveFile(t, tempOutFile)
+
+		err := Copy(realInputFile, tempOutFile, 10000, 0)
+
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("Check default behaviour", func(t *testing.T) {
+		defer helperRemoveFile(t, tempOutFile)
+
+		Copy(realInputFile, tempOutFile, 0, 0)
+
+		fInputSize := getFileSize(realInputFile)
+		fOutSize := getFileSize(tempOutFile)
+
+		require.Equal(t, fInputSize, fOutSize)
+	})
+
+	limits := []int64{10, 1000, 1025, 10000}
+
+	for _, limit := range limits {
+		t.Run("Check behaviour with 'limit' value", func(t *testing.T) {
+			defer helperRemoveFile(t, tempOutFile)
+
+			Copy(realInputFile, tempOutFile, 0, limit)
+
+			fInputSize := getFileSize(realInputFile)
+			if limit > fInputSize {
+				limit = fInputSize
+			}
+
+			fOutSize := getFileSize(tempOutFile)
+
+			require.Equal(t, limit, fOutSize)
+		})
+	}
+
+	offsetAndLimits := []struct {
+		offset int64
+		limit  int64
+	}{
+		{100, 1000},
+		{6000, 10000},
+		{-1, 1000},
+		{100, -1},
+		{-100, -100},
+	}
+
+	for _, ol := range offsetAndLimits {
+		t.Run("Check behaviour with 'offset' and 'limit' values", func(t *testing.T) {
+			defer helperRemoveFile(t, tempOutFile)
+
+			Copy(realInputFile, tempOutFile, ol.offset, ol.limit)
+
+			fInputSize := getFileSize(realInputFile)
+
+			expectedSize := fInputSize
+			if ol.offset > 0 {
+				expectedSize = fInputSize - ol.offset
+			}
+			if expectedSize > ol.limit {
+				if ol.limit > 0 {
+					expectedSize = ol.limit
+				}
+			}
+
+			fOutSize := getFileSize(tempOutFile)
+
+			require.Equal(t, expectedSize, fOutSize)
+		})
+	}
+}
+
+func helperRemoveFile(tb testing.TB, removeFilePath string) {
+	tb.Helper()
+	os.Remove(removeFilePath)
+}
+
+func getFileSize(fPath string) int64 {
+	info, err := os.Stat(fPath)
+	if err != nil {
+		return 0
+	}
+	return info.Size()
 }

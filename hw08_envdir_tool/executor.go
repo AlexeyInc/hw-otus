46e6b1a47225cb36ec3035eca11aae760e2664b0
Alexey Ovsianicoff <allah420@protonmail.com>
2022-02-17 20:27:40 +0200

    Added base logic

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,31 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+
+	for k, ev := range env {
+		if env[k].NeedRemove {
+			os.Unsetenv(k)
+		}
+		if ev.Value != "" {
+			os.Setenv(k, ev.Value)
+		}
+	}
+
+	resCmd := exec.Command(cmd[0], cmd[1])
+	resCmd.Env = os.Environ()
+	resCmd.Env = append(resCmd.Env, "ADDED=from original env")
+	resCmd.Args = append(resCmd.Args, cmd[2], cmd[3])
+	resCmd.Stdout = os.Stdout
+
+	if err := resCmd.Run(); err != nil {
+		return 0
+	}
+
+	return 1
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	//
+	// "./testdata/env"
+	envVariables, err := ReadDir(os.Args[1])
+
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+
+	//
+	// []string{"/bin/bash", "./testdata/echo.sh", "arg1=1", "arg2=2"}
+	res := RunCmd(os.Args[2:], envVariables)
+
+	os.Exit(res)
 }

de2727dbe704b79772d0099d0f356425457088dc
Alexey Ovsianicoff <allah420@protonmail.com>
2022-03-16 11:33:42 +0200

    first version
    all tests (except test.sh) passed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,81 @@
 package main
 
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+// $ go-telnet --timeout=10s host port
+// $ go-telnet mysite.ru 8080
+// $ go-telnet --timeout=3s 1.1.1.1 123
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	host := os.Args[1]
+	port := os.Args[2]
+
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", 2*time.Minute, "Default max time 2min")
+
+	address := net.JoinHostPort(host, port)
+
+	// ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	// defer stop()
+
+	serverDisconnected := make(chan bool)
+	//clientDisconnected := make(chan bool)
+	defer close(serverDisconnected)
+	//defer close(clientDisconnected)
+
+	reader := os.Stdin
+
+	client := NewTelnetClient(address, timeout, reader, &bytes.Buffer{})
+
+	if err := client.Connect(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+	fmt.Printf("...Connected to %s:%s\n", host, port)
+	defer client.Close()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+	OUT:
+		for {
+			err := client.Receive()
+			if err != nil {
+				serverDisconnected <- true
+				break OUT
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+	OUT:
+		for {
+			err := client.Send()
+			if err != nil {
+				fmt.Println("...Connection was closed by client")
+				wg.Done()
+				break OUT
+			}
+			select {
+			//case <-ctx.Done():
+			case <-serverDisconnected:
+				fmt.Println("...Connection was closed by peer")
+				break OUT
+			default:
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,9 +16,66 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Client struct {
+	Network string
+	address string
+	TimeD   time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Client{
+		"tcp",
+		address,
+		timeout,
+		in,
+		out,
+		nil,
+	}
+}
+
+func (client *Client) Connect() error {
+	conn, err := net.DialTimeout(client.Network, client.address, client.TimeD)
+	if err != nil {
+		return err
+	}
+	client.conn = conn
+	return err
+}
+
+func (client *Client) Close() error {
+	if err := client.conn.Close(); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (client *Client) Send() error {
+	scanner := bufio.NewScanner(client.in)
+
+	if scanner.Scan() {
+		msgToServer := scanner.Text()
+		//fmt.Println(msgToServer)
+
+		client.conn.Write([]byte(fmt.Sprintf("%s\n", msgToServer)))
+		return nil
+	}
+
+	return errors.New("error: unable to scan input")
+}
+
+func (client *Client) Receive() error {
+	scanner := bufio.NewScanner(client.conn)
+
+	if scanner.Scan() {
+		msgFromSever := scanner.Text()
+		fmt.Println(msgFromSever)
+		client.out.Write([]byte(fmt.Sprintf("%s\n", msgFromSever)))
+		return nil
+	}
+	return errors.New("error: unable to scan msg from server")
 }
 
 // Place your code here.

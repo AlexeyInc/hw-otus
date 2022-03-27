92650d03df33a5dc31242490874b08c22f8ad277
Alexey Ovsianicoff <allah420@protonmail.com>
2022-03-27 14:28:01 +0300

    Commented tetst
    Wait for lection with Docker

--- hw12_13_14_15_calendar/internal/storage/sql/sqlc/main_test.go
+++ hw12_13_14_15_calendar/internal/storage/sql/sqlc/main_test.go
@@ -1,41 +1,41 @@
 package sqlcstorage
 
-import (
-	"database/sql"
-	"errors"
-	"log"
-	"os"
-	"testing"
+// import (
+// 	"database/sql"
+// 	"errors"
+// 	"log"
+// 	"os"
+// 	"testing"
 
-	"github.com/AlexeyInc/hw-otus/hw12_13_14_15_calendar/configs"
-	_ "github.com/lib/pq"
-)
+// 	"github.com/AlexeyInc/hw-otus/hw12_13_14_15_calendar/configs"
+// 	_ "github.com/lib/pq"
+// )
 
-var testQueries *Queries
+// var testQueries *Queries
 
-var (
-	configPath      = "./configs/config.toml"
-	configLocalPath = "../../../../configs/config.toml"
-)
+// var (
+// 	configPath      = "./configs/config.toml"
+// 	configLocalPath = "../../../../configs/config.toml"
+// )
 
-func TestMain(m *testing.M) {
-	config, err := configs.NewConfig(configLocalPath)
-	if errors.Is(err, os.ErrNotExist) {
-		config, err = configs.NewConfig(configPath)
-	}
-	if err != nil {
-		log.Fatalln("can't read config file: " + err.Error())
-	}
-	testDB, err := sql.Open(config.Storage.Driver, config.Storage.Source)
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
+// func TestMain(m *testing.M) {
+// 	config, err := configs.NewConfig(configLocalPath)
+// 	if errors.Is(err, os.ErrNotExist) {
+// 		config, err = configs.NewConfig(configPath)
+// 	}
+// 	if err != nil {
+// 		log.Fatalln("can't read config file: " + err.Error())
+// 	}
+// 	testDB, err := sql.Open(config.Storage.Driver, config.Storage.Source)
+// 	if err != nil {
+// 		log.Fatal("cannot connect to db:", err)
+// 	}
 
-	testQueries = New(testDB)
+// 	testQueries = New(testDB)
 
-	code := m.Run()
+// 	code := m.Run()
 
-	deleteAllTestEvents()
+// 	deleteAllTestEvents()
 
-	os.Exit(code)
-}
+// 	os.Exit(code)
+// }

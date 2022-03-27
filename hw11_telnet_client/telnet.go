3c21c09c34f659da1542f25ab5e022cb28460ca5
Alexey Ovsianicoff <allah420@protonmail.com>
2022-03-27 15:44:26 +0300

    Remove comments

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -24,12 +24,7 @@ type Client struct {
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	return &Client{
-		"tcp",
-		address,
-		timeout,
-		in,
-		out,
-		nil,
+		"tcp", address, timeout, in, out, nil,
 	}
 }
 
@@ -48,35 +43,10 @@ func (client *Client) Close() error {
 
 func (client *Client) Send() error {
 	_, err := io.Copy(client.conn, client.in)
-
 	return err
-	// scanner := bufio.NewScanner(client.in)
-
-	// if scanner.Scan() {
-	// 	msgToServer := scanner.Text()
-	// 	// fmt.Println(msgToServer)
-
-	// 	client.conn.Write([]byte(fmt.Sprintf("%s\n", msgToServer)))
-	// 	return nil
-	// }
-
-	//return errors.New("error: unable to scan input")
 }
 
 func (client *Client) Receive() error {
 	_, err := io.Copy(client.out, client.conn)
-
 	return err
-	// scanner := bufio.NewScanner(client.conn)
-
-	// if scanner.Scan() {
-	// 	msgFromSever := scanner.Text()
-	// 	fmt.Println(msgFromSever)
-	// 	client.out.Write([]byte(fmt.Sprintf("%s\n", msgFromSever)))
-	// 	return nil
-	// }
-	// return errors.New("error: unable to scan msg from server")
 }
-
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
